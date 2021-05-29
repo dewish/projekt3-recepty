bbf85aee4f6a1f9552241e6bf39b2e66d8b057b8
Lenka Harastova <lenca.harastova@gmail.com>
2021-05-29 21:28:05 +0200

    first_draft

--- app.js
+++ app.js
@@ -17,3 +17,86 @@ recept-hodnoceni, recept-nazev, recept-popis.
 
 6) Poslední vybraný recept ulož do Local Storage, aby se při novém otevření aplikace načetl.
 */
+
+/////////////////////////////////////////////
+// PARSE DATA FROM JSON
+
+function loadJSON(path, success, error) {
+  let request = new XMLHttpRequest();
+  request.onreadystatechange = function () {
+    if (request.readyState === XMLHttpRequest.DONE) {
+      if (request.status === 200) {
+        if (success) success(JSON.parse(request.responseText));
+      } else {
+        if (error) error(request);
+      }
+    }
+  };
+  request.open("GET", path, true);
+  request.send();
+}
+
+/////////////////////////////////////////////
+// DATA FROM JSON
+
+const receptyElement = document.querySelector("#recepty");
+
+loadJSON(
+  "recepty.json",
+
+  function (receptyData) {
+    function receptyList(recepty) {
+      return `      
+            <div class="recept">
+                <div class="recept-obrazek">
+                    <img src="${recepty.img}" alt="Obrazek">
+                </div>
+                <div class="recept-info" id="${recepty.nadpis}">
+                    <h3>${recepty.nadpis}</h3>
+                </div>
+            </div>
+                    `;
+    }
+
+    function receptyInfo(recepty) {
+      return `      
+        <div class="recept-detail-obrazek">
+          <img id="recept-foto" src="${recepty.img}" alt="" />
+        </div>
+        <div class="recept-detail-info">
+          <header>
+            <div class="recept-kategorie">
+              <span class="fas fa-tag"></span> Kategorie:
+              <span class="hodnota" id="recept-kategorie"
+                >${recepty.kategorie}</span>
+            </div>
+            <div class="recept-hodnoceni">
+              <span class="far fa-star"></span>
+              <span class="hodnota" id="recept-hodnoceni"
+                >${recepty.hodnoceni}</span>
+            </div>
+          </header>
+          <h1 id="recept-nazev">${recepty.nadpis}</h1>
+          <p id="recept-popis">${recepty.popis}</p>
+        </div>
+       `;
+    }
+
+    document.getElementById("recepty_list").innerHTML = `
+      <div>  
+        ${receptyData.map(receptyList).join("")}</div> `;
+
+    receptyElement.addEventListener("click", (e) => {
+      let selectedRecept = receptyData.find(
+        (recept) => recept.nadpis === e.target.id
+      );
+
+      document.getElementById("deatil").innerHTML = `
+        <div> ${receptyInfo(selectedRecept)}</div> `;
+    });
+  },
+
+  function (xhr) {
+    console.error(xhr);
+  }
+);
